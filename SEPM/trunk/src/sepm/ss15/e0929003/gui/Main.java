69896a43dc26673c942ba4e7dde44b96151c13e3
u0929003 <u0929003@4b74d27c-1d9a-47e4-a267-0ed51ee71218>
2015-03-23 12:42:18 +0000

    Basisstruktur für die GUI entworfen (Jockeys und Horses).

--- SEPM/trunk/src/sepm/ss15/e0929003/gui/Main.java
+++ SEPM/trunk/src/sepm/ss15/e0929003/gui/Main.java
@@ -14,14 +14,16 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
+        primaryStage.setTitle("Wendy’s Rennpferde");
+        primaryStage.setMinHeight(500);
+        primaryStage.setMinWidth(750);
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
-        //launch(args);
+        launch(args);
     }
 }
