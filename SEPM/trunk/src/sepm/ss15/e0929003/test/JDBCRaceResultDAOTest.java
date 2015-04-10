df05efcc6b8490847c855ffdf92e32102c346400
u0929003 <u0929003@4b74d27c-1d9a-47e4-a267-0ed51ee71218>
2015-04-10 17:09:55 +0000

    Jetzt wird Sequence verwendet um die neue ID des Rennens zu bekommen.

--- SEPM/trunk/src/sepm/ss15/e0929003/test/JDBCRaceResultDAOTest.java
+++ SEPM/trunk/src/sepm/ss15/e0929003/test/JDBCRaceResultDAOTest.java
@@ -13,11 +13,11 @@ public class JDBCRaceResultDAOTest extends AbstractRaceResultDAOTest {
     @Before
     public void setUp() throws DAOException, SQLException {
         setRaceResultDAO(new JDBCRaceResultDAO());
-        RaceResult raceResult1 = new RaceResult(5,1,1,"Jacky","Eddie Ahern",60.0,0.95,2.48,241.36,2);
-        RaceResult raceResult2 = new RaceResult(5,2,2,"Avenida","Robby Albarado",70.0,1.05,1.54,131.19,4);
-        RaceResult raceResult3 = new RaceResult(5,3,3,"Manchego","Steve Cauthen",77.0,1.00,-0.04,-3.08,5);
-        RaceResult raceResult4 = new RaceResult(5,4,4,"Trixie","Patricia Cooksey",80.0,0.97,2.85,221.16,3);
-        RaceResult raceResult5 = new RaceResult(5,5,5,"Dark Sparks","Alan Garcia",90.0,1.02,3.15,289.17,1);
+        RaceResult raceResult1 = new RaceResult(null,1,1,"Jacky","Eddie Ahern",60.0,0.95,2.48,241.36,2);
+        RaceResult raceResult2 = new RaceResult(null,2,2,"Avenida","Robby Albarado",70.0,1.05,1.54,131.19,4);
+        RaceResult raceResult3 = new RaceResult(null,3,3,"Manchego","Steve Cauthen",77.0,1.00,-0.04,-3.08,5);
+        RaceResult raceResult4 = new RaceResult(null,4,4,"Trixie","Patricia Cooksey",80.0,0.97,2.85,221.16,3);
+        RaceResult raceResult5 = new RaceResult(null,5,5,"Dark Sparks","Alan Garcia",90.0,1.02,3.15,289.17,1);
         setRaceResults(raceResult1,raceResult2,raceResult3,raceResult4,raceResult5);
         JDBCSingletonConnection.reset(JDBCRaceResultDAOTest.class.getClassLoader().getResource("res/testdata.sql").getPath().substring(1));
 
