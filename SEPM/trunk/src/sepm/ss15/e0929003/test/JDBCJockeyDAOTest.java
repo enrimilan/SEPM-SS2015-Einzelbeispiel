a7f7f1e7110c2ad1b3b8fc469300a0151f83e701
Enri Miho <enrimilan@users.noreply.github.com>
2015-04-11 21:44:50 +0000

    Junit: Datenbank resetten nachdem Tests fertig sind. AssertEquals statt AssertTrue verwenden(schönere Fehlerausgabe).

--- SEPM/trunk/src/sepm/ss15/e0929003/test/JDBCJockeyDAOTest.java
+++ SEPM/trunk/src/sepm/ss15/e0929003/test/JDBCJockeyDAOTest.java
@@ -1,6 +1,8 @@
 package sepm.ss15.e0929003.test;
 
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import sepm.ss15.e0929003.dao.DAOException;
 import sepm.ss15.e0929003.dao.JDBCJockeyDAO;
 import sepm.ss15.e0929003.dao.JDBCSingletonConnection;
@@ -10,6 +12,12 @@ import java.sql.SQLException;
 
 public class JDBCJockeyDAOTest extends AbstractJockeyDAOTest {
 
+    @BeforeClass
+    public static void resetBefore() throws DAOException {
+        JDBCSingletonConnection.getConnection();
+        JDBCSingletonConnection.reset(JDBCJockeyDAOTest.class.getClassLoader().getResource("res/testdata.sql").getPath().substring(1));
+    }
+
     @Before
     public void setUp() throws DAOException, SQLException {
         setJockeyDAO(new JDBCJockeyDAO());
@@ -17,8 +25,10 @@ public class JDBCJockeyDAOTest extends AbstractJockeyDAOTest {
         Jockey jockeyWithSkillNull = new Jockey(null,"Test Jockey ","2","Country",null,false);
         Jockey jockeyWithNegativeId = new Jockey(-1,"Test Jockey ","3","Country",-1.0,false);
         setJockeys(validJockey, jockeyWithSkillNull, jockeyWithNegativeId);
-        JDBCSingletonConnection.reset(JDBCJockeyDAOTest.class.getClassLoader().getResource("res/testdata.sql").getPath().substring(1));
-
     }
-    
+
+    @AfterClass
+    public static void resetAfter() throws DAOException {
+        JDBCSingletonConnection.reset(JDBCJockeyDAOTest.class.getClassLoader().getResource("res/testdata.sql").getPath().substring(1));
+    }
 }
